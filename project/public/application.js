486e42c7da9442a1aa659aeb4691bab9dfa0b8de
Elena <elena.15@hotmail.it>
2015-01-13 18:13:49 +0000

    user can search for contacts

--- project/public/application.js
+++ project/public/application.js
@@ -1,4 +1,4 @@
-function eachWord(str){
+function capitalizeFirstLetter(str){
   return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
@@ -6,7 +6,7 @@ $(document).ready(function(){
   $.get('http://fast-gorge.herokuapp.com/contacts', function(data){
 
     $.each(data, function(i){
-      $('.contacts').append("<li class='contact'>" + data[i].surname + " " + data[i].first_name +
+      $('.contacts').append("<li class=contact>" + data[i].surname + " " + data[i].first_name +
       " " + data[i].address + " " + data[i].phone_number + " " + data[i].email + "</li>")
     })
 
@@ -15,8 +15,24 @@ $(document).ready(function(){
     }).appendTo('ul')
 
     $('#search_button').on('click', function(){
-      var search_name = $('#search').val()
-      alert(eachWord(search_name))
+      var search_name = capitalizeFirstLetter($('#search').val())
+      console.log(search_name)
+
+      $(".contacts li").each(function(){
+        var text = $(this).text()
+
+        // If the list item does not contain the text phrase fade it out
+        if (text.indexOf(search_name) === -1) { // use the variable here
+          $(this).hide();
+
+          // Show the list item if the phrase matches and increase the count by 1
+        } else {
+          $(this).show();
+          // count++;
+        }
+        console.log('hi')
+
+      });
     })
 
   })

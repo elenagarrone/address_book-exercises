33f24d2541c410df77f3a7bf0096cd8eb0665254
Elena <elena.15@hotmail.it>
2015-01-13 15:53:24 +0000

    i can see the info of each user

--- project/public/application.js
+++ project/public/application.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
   $.get('http://fast-gorge.herokuapp.com/contacts', function(data){
+
     $.each(data, function(i){
-      $('.contacts').append("<li class='contact'>" + data[i].surname + " " + data[i].first_name + "</li>")
+      $('.contacts').append("<li class='contact'>" + data[i].surname + " " + data[i].first_name +
+      " " + data[i].address + " " + data[i].phone_number + " " + data[i].email + "</li>")
     })
+
     $('li').sort(function(a,b){
       return $( a ).text() > $( b ).text();
     }).appendTo('ul')
+
   })
 })
--- project/spec/features/contacts_spec.rb
+++ project/spec/features/contacts_spec.rb
@@ -9,7 +9,15 @@ feature 'The list of the contacts' do
 
   scenario 'in alphabetical order', js: true do
     visit '/'
+    sleep(1)
     expect(first('.contact')).to have_content('Bannon')
+  end
+
+  scenario 'with the informations', js: true do
+    visit '/'
+    expect(page).to have_content('Banana Street')
+    expect(page).to have_content('07411857962')
+    expect(page).to have_content('Barry@banana.org')
   end
 
 end

f7bf2e48476b9b03d9156be1931acf46c7ab3442
Elena <elena.15@hotmail.it>
2015-01-14 09:48:06 +0000

    li has id of the contact

--- project/public/contacts.js
+++ project/public/contacts.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
   $.get('http://fast-gorge.herokuapp.com/contacts', function(data){
 
     $.each(data, function(i){
-      $('.contacts').append("<li class=contact>" + data[i].surname + " " + data[i].first_name +
+      $('.contacts').append("<li class=" + data[i].id + ">" + data[i].surname + " " + data[i].first_name +
       " " + data[i].address + " " + data[i].phone_number + " " + data[i].email + "</li>")
     })
 

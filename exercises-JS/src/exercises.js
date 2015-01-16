e69d9f6f5daebf694cb3f4005a9fb44a46d546ce
Elena <elena.15@hotmail.it>
2015-01-16 13:45:49 +0000

    seventh exercise done

--- exercises-JS/src/exercises.js
+++ exercises-JS/src/exercises.js
@@ -60,3 +60,28 @@ var Hero = {
 // ======EXERCISE 6======
 var getLife = Hero.getLife;
 var LinksLife = getLife.bind(Hero);
+
+
+// ======EXERCISE 7======
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+    fToBind = this,
+    fNOP = function () {},
+    fBound = function () {
+      return fToBind.apply(this instanceof fNOP && oThis
+        ? this
+        : oThis,
+        aArgs.concat(Array.prototype.slice.call(arguments)));
+      };
+
+      fNOP.prototype = this.prototype;
+      fBound.prototype = new fNOP();
+
+      return fBound;
+    };
+  }

1b97332d6b7e5e9e38d8d8b97025bef304c274aa
Elena <elena.15@hotmail.it>
2015-01-16 10:49:23 +0000

    second exercise done

--- exercises-JS/spec/exercisesSpec.js
+++ exercises-JS/spec/exercisesSpec.js
@@ -1,12 +1,26 @@
-describe('camelCase', function(){
+describe('Exercise1', function(){
 
-  describe('Takes a string like:', function(){
+  describe('The function camelCase(string) takes a string like:', function(){
     it("'your soul is mine' and returns 'yourSoulIsMine'", function(){
       expect(camelCase('your soul is mine')).toEqual('yourSoulIsMine');
     })
 
     it("' Get over   here! ' and returns 'getOverHere!'", function(){
       expect(camelCase(' Get over   here! ')).toEqual('getOverHere!');
+    })
+
+  })
+})
+
+describe('Exercise2', function(){
+
+  describe('The function string.camelCase takes a string like:', function(){
+    it("'your soul is mine' and returns 'yourSoulIsMine'", function(){
+      expect('your soul is mine'.camelCase()).toEqual('yourSoulIsMine');
+    })
+
+    it("' Get over   here! ' and returns 'getOverHere!'", function(){
+      expect(' Get over   here! '.camelCase()).toEqual('getOverHere!');
     })
 
   })
--- exercises-JS/src/exercises.js
+++ exercises-JS/src/exercises.js
@@ -8,3 +8,11 @@ function camelCase(string) {
       return index === 0 ? match.toLowerCase() : match.toUpperCase();
     });
 }
+
+String.prototype.camelCase = function() {
+  var string = this.trim();
+  return string.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
+    if (+match === 0) return "";
+    return index === 0 ? match.toLowerCase() : match.toUpperCase();
+  });
+}
